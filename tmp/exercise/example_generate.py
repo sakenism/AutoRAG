7f940868d4e858cc3bd2ce32bc88480b8161800c
Saken Mukanov <saken.mukanov@nu.edu.kz>
2025-06-02 12:46:25 +0500

    whole list

---
+++ tmp/exercise/example_generate.py
@@ -0,0 +1,5 @@
+from autorag.evaluator import Evaluator
+
+evaluator = Evaluator(qa_data_path='qa_test.parquet', corpus_data_path='corpus.parquet',
+                      project_dir='dir')
+evaluator.start_trial('config_generation.yaml')
