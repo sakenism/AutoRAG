1226e8d89e46ade396eacedd74cc7c7391d548d4
Saken Mukanov <saken.mukanov@nu.edu.kz>
2025-06-05 16:12:25 +0500

    new df

--- tmp/exercise/example_retrieve.py
+++ tmp/exercise/example_retrieve.py
@@ -1,5 +1,5 @@
 from autorag.evaluator import Evaluator
 
-evaluator = Evaluator(qa_data_path='qa_test.parquet', corpus_data_path='corpus.parquet',
+evaluator = Evaluator(qa_data_path='qa_test_test.parquet', corpus_data_path='corpus_test.parquet',
                       project_dir='dir')
 evaluator.start_trial('config_retrieval.yaml')
